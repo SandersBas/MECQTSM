0665dee590a370765000a459477bbcc4763d7032
SandersBas <109312438+SandersBas@users.noreply.github.com>
2025-11-24 10:39:53 +0100

    Delete Default approach toolkit/estim.m

--- Default approach toolkit/estim.m
+++
@@ -1,9 +0,0 @@
-function [theta_tilde, Sigma_tilde] = estim(F_tilde)
-% Input: 
-%   F_tilde: vector of noisy data
-% Output: 
-%   theta_tilde: structural estimator
-%   Sigma_tilde: consistent estimator of the sampling variance
-
-% Add code here
-end
