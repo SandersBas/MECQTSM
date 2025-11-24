2894a9b0e343f888e688ae8e8feb14ffb4d8b869
SandersBas <109312438+SandersBas@users.noreply.github.com>
2025-11-24 10:39:08 +0100

    Delete Default approach toolkit/g.m

--- Default approach toolkit/g.m
+++
@@ -1,9 +0,0 @@
-function [gamma] = g(F, theta)
-% Input: 
-%   F: vector of true data
-%   theta: structural parameter
-% Output: 
-%   gamma: scalar counterfactual prediction
-
-% Add code here
-end
