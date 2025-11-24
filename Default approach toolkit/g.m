2f19ddeaffef0213a56c3f150e4ce3ebc12c745b
SandersBas <109312438+SandersBas@users.noreply.github.com>
2025-11-24 10:39:39 +0100

    Add files via upload

---
+++ Default approach toolkit/g.m
@@ -0,0 +1,9 @@
+function [gamma] = g(F, theta)
+% Input: 
+%   F: vector of true data
+%   theta: structural parameter
+% Output: 
+%   gamma: scalar counterfactual prediction
+
+% Add code here
+end
