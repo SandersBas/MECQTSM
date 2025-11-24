b8f7aa2747c3be549c12f68f1e99797de060fe00
SandersBas <109312438+SandersBas@users.noreply.github.com>
2025-11-24 10:38:03 +0100

    Delete General algorithm toolkit/g.m

--- General algorithm toolkit/g.m
+++
@@ -1,9 +0,0 @@
-function [gamma] = g(D, theta)
-% Input: 
-%   D: vector of true data
-%   theta: structural parameter
-% Output: 
-%   gamma: scalar counterfactual prediction
-
-% Add code here
-end
